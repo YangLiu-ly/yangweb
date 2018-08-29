02f844908dd434512ceb3191a27f1d5f2ac35082
yangyang <1309833753@qq.com>
2018-08-29 15:08:12 +0800

    添加项目

---
+++ test/轮播图/js/data.js
@@ -0,0 +1,22 @@
+
+/**
+ * 获取数据
+ */
+function getData (callback) {
+    callback([{
+        title: '神奇的九寨',
+        img: '../../imgs/timg.jpg'
+    }, {
+        title: '呼伦贝尔大草原',
+        img: '../../imgs/timg1.jpg'
+    }, {
+        title: '青藏高原',
+        img: '../../imgs/timg2.jpg'
+    }, {
+        title: '珠穆朗玛峰',
+        img: '../../imgs/timg3.jpg'
+    }, {
+        title: '珠穆朗玛峰aaa',
+        img: '../../imgs/timg1.jpg'
+    }]);
+}
